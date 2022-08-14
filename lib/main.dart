8c952196e2609196d685695dc85d0d065acbce46
esentis <esentakos@yahoo.gr>
2022-08-14 19:26:13 +0300

    🔥  Removes debug flag

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Tzoker Generator',
+      debugShowCheckedModeBanner: false,
       home: LandingPage(title: 'Flutter Demo Home Page'),
     );
   }

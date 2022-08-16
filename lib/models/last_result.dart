8a15c3882f3610ebbdf1bbb490f156fb602a04f5
esentis <esentakos@yahoo.gr>
2022-08-16 23:10:55 +0300

    ✨ Creates last result model

---
+++ lib/models/last_result.dart
@@ -0,0 +1,13 @@
+class LastResult {
+  LastResult({
+    required this.date,
+    required this.tzoker,
+    required this.winningNumbers,
+    required this.sortedWinningNumbers,
+  });
+
+  final DateTime date;
+  final List winningNumbers;
+  final int tzoker;
+  final List sortedWinningNumbers;
+}

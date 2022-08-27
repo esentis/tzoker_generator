cc97a2c712e147988714c77fd87844348f2c619a
esentis <esentakos@yahoo.gr>
2022-08-27 17:54:20 +0300

    🔧 Setups supabase on startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:tzoker_generator/screens/landing_page.dart';
 import 'package:tzoker_generator/screens/statistics.dart';
 
 Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://qvliopsxcffejpcxxmfb.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2bGlvcHN4Y2ZmZWpwY3h4bWZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAzMjk3MzksImV4cCI6MTk3NTkwNTczOX0.OshaOvSwzZ2Fgtj9kAqL_COmgTfBlnGHmV43EAyDja4',
+    // authCallbackUrlHostname: 'login-callback', // optional
+    debug: true // optional
+    ,
+  );
   runApp(const MyApp());
 }
 

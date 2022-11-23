40423ebd6e876022c94e6f2a59f6337645c40f5d
esentis <esentakos1@gmail.com>
2022-11-23 18:04:33 +0200

    ♻️ Refactors stats divider to seperate widget

---
+++ lib/widgets/stats_divider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class StatsDivider extends StatelessWidget {
+  const StatsDivider({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        SizedBox(
+          height: 4,
+        ),
+        SizedBox(
+          width: 350,
+          child: Divider(
+            color: Colors.red,
+          ),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+      ],
+    );
+  }
+}
